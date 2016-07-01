7c3fe60191d39a71647fd2ea60e1d565487d8615
jeremyletang <letang.jeremy@gmail.com>
2016-07-01 17:44:49 +0100

    update package file to publish in crates.io

--- src/colorize.rs
+++ src/colorize.rs
@@ -19,48 +19,44 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/*!
-Terminal color using ansi escape character for Rust.
-
-```Rust
-extern mod colorize;
-// Import the trait implemented for &'static str and ~str
-use colorize::AnsiColor;
-// Import the colors for the global
-use colorize::{BrightRed, Blue};
-
-pub fn main() {
-    // Set some global colors
-    colorize::global_fg(BrightRed);
-    colorize::global_bg(Blue);
-    // ^~~~ These settings are reset to default at the end.
-
-    // You can use specific colors or style on a given str,
-    // the globals colors are restored after !
-
-    // Write a green underlined text on a yellow background !
-    println!("{}", "Hello World !".green().underline().yellowb());
-
-    // Use bright or normal colors
-    println!("{}", "Bright Green foreground and Magenta background !"
-                   .b_green()
-                   .magentab());
-}
-
-```
-*/
+// Terminal color using ansi escape character for Rust.
+//
+// ```Rust
+// extern mod colorize;
+// // Import the trait implemented for &'static str and ~str
+// use colorize::AnsiColor;
+// // Import the colors for the global
+// use colorize::{BrightRed, Blue};
+//
+// pub fn main() {
+//     // Set some global colors
+//     colorize::global_fg(BrightRed);
+//     colorize::global_bg(Blue);
+//     // ^~~~ These settings are reset to default at the end.
+//
+//     // You can use specific colors or style on a given str,
+//     // the globals colors are restored after !
+//
+//     // Write a green underlined text on a yellow background !
+//     println!("{}", "Hello World !".green().underline().yellowb());
+//
+//     // Use bright or normal colors
+//     println!("{}", "Bright Green foreground and Magenta background !"
+//                    .b_green()
+//                    .magentab());
+// }
+//
+// ```
 
 #![crate_name = "colorize"]
 #![crate_type = "dylib"]
 #![crate_type = "rlib"]
-#![allow(unstable)]
 
 use Color::*;
 use BgColor::*;
 use Style::*;
 
 use std::mem;
-use std::str::FromStr;
 
 /// Ansi color to set the global foreground / background color
 #[derive(Clone, Copy)]
